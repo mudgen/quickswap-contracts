42c22894d30ad665571b3c07e8dc34808cd608a0
mudgen <nick@perfectabstractions.com>
2021-12-04 18:11:27 -0500

    implemented QuickswapV1Router01.sol

--- contracts/QuickswapV1Router01.sol
+++ contracts/QuickswapV1Router01.sol
@@ -27,15 +27,42 @@ contract QuickswapV1Router01 is UniswapV2Router02(0x5757371414417b8C6CAad45bAeF9
         TransferHelper.safeTransferFrom(tokenA, msg.sender, pair, amountA);
         TransferHelper.safeTransferFrom(tokenB, msg.sender, pair, amountB);
         liquidity = IUniswapV2Pair(pair).mint(address(this));
-        if(IUniswapV2Pair(pair).allowance(msg.sender, rewardPool) < liquidity) {
+        if(IUniswapV2Pair(pair).allowance(address(this), rewardPool) < liquidity) {
             IUniswapV2Pair(pair).approve(rewardPool, uint256(-1));
         }
         IStakingReward(rewardPool).stake(liquidity, to);
     }
 
-
-
-    
+    function addLiquidityETHAndStake(
+        address token,
+        uint amountTokenDesired,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline,
+        address rewardPool
+    ) external payable ensure(deadline) returns (uint amountToken, uint amountETH, uint liquidity) {
+        (amountToken, amountETH) = _addLiquidity(
+            token,
+            WETH,
+            amountTokenDesired,
+            msg.value,
+            amountTokenMin,
+            amountETHMin
+        );
+        address pair = UniswapV2Library.pairFor(factory, token, WETH);
+        require(pair == IStakingReward(rewardPool).stakingToken(), "Pair is not staking token");
+        TransferHelper.safeTransferFrom(token, msg.sender, pair, amountToken);
+        IWETH(WETH).deposit{value: amountETH}();
+        assert(IWETH(WETH).transfer(pair, amountETH));
+        liquidity = IUniswapV2Pair(pair).mint(address(this));
+        // refund dust eth, if any
+        if (msg.value > amountETH) TransferHelper.safeTransferETH(msg.sender, msg.value - amountETH);
+        if(IUniswapV2Pair(pair).allowance(address(this), rewardPool) < liquidity) {
+            IUniswapV2Pair(pair).approve(rewardPool, uint256(-1));
+        }
+        IStakingReward(rewardPool).stake(liquidity, to);
+    }
 
     function unstakeAndRemoveLiquidity(
         address tokenA,
@@ -45,7 +72,7 @@ contract QuickswapV1Router01 is UniswapV2Router02(0x5757371414417b8C6CAad45bAeF9
         address to,
         uint deadline,
         address rewardPool
-    ) external virtual ensure(deadline) returns (uint amountA, uint amountB) {        
+    ) public ensure(deadline) returns (uint amountA, uint amountB) {        
         address pair = UniswapV2Library.pairFor(factory, tokenA, tokenB);
         require(pair == IStakingReward(rewardPool).stakingToken(), "Pair is not staking token");
         // IUniswapV2Pair(pair).transferFrom(msg.sender, pair, liquidity); // send liquidity to pair
@@ -57,8 +84,49 @@ contract QuickswapV1Router01 is UniswapV2Router02(0x5757371414417b8C6CAad45bAeF9
         require(amountB >= amountBMin, 'UniswapV2Router: INSUFFICIENT_B_AMOUNT');
     }
 
+    function unstakeAndRemoveLiquidityETH(
+        address token,        
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline,
+        address rewardPool
+    ) external ensure(deadline) returns (uint amountToken, uint amountETH) {
+        (amountToken, amountETH) = unstakeAndRemoveLiquidity(
+            token,
+            WETH,            
+            amountTokenMin,
+            amountETHMin,
+            address(this),
+            deadline,
+            rewardPool
+        );
+        TransferHelper.safeTransfer(token, to, amountToken);
+        IWETH(WETH).withdraw(amountETH);
+        TransferHelper.safeTransferETH(to, amountETH);
+    }     
+
+    // **** REMOVE LIQUIDITY (supporting fee-on-transfer tokens) ****
+    function unstakeAndRemoveLiquidityETHSupportingFeeOnTransferTokens(
+        address token,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline,
+        address rewardPool
+    ) external ensure(deadline) returns (uint amountETH) {
+        (, amountETH) = unstakeAndRemoveLiquidity(
+            token,
+            WETH,            
+            amountTokenMin,
+            amountETHMin,
+            address(this),
+            deadline,
+            rewardPool
+        );
+        TransferHelper.safeTransfer(token, to, IERC20(token).balanceOf(address(this)));
+        IWETH(WETH).withdraw(amountETH);
+        TransferHelper.safeTransferETH(to, amountETH);
+    }
     
-
-
-
 }
